9594454a2342cba83fcd8a5ddd8f64ddd3529aaf
Aadithya-Ram <68803605+Aadithya-Ram@users.noreply.github.com>
2020-10-03 11:36:20 +0530

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -1,21 +1,11 @@
-class bird {
-    constructor(x,y){
-        this.body = Bodies.rectangle (x,y,50,50)
-        this.width = 50
-        this.height = 50 
-        World.add(world, this.body)
-    }
-display() {
- var pos = this.body.position
- pos.x = mouseX
- pos.y = mouseY
- var angle = this.body.angle
-    push();
-    translate (pos.x, pos.y);
-    rotate(angle)
-    rectMode(CENTER)
-    fill("blue");
-    rect(0, 0, this.width, this.height);
-    pop();
+class Bird extends BaseClass{
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+  }
+  display(){
+    this.body.position.x = mouseX;
+    this.body.position.y = mouseY;
+    super.display();
+  }
 }
-};
--- Log.js
+++ Log.js
@@ -1,26 +1,7 @@
-class log {
+class Log extends BaseClass {
     constructor(x, y, height, angle) {
-      var options = {
-          'restitution':0.8,
-          'friction':1.0,
-          'density':1.0
-      }
-      this.body = Bodies.rectangle(x, y, 10, height, options);
-      this.width = 10;
-      this.height = height;
-      Matter.Body.setAngle(this.body, angle)
-      World.add(world, this.body);
+      super(x, y, 20, height);
+      Matter.Body.setAngle(this.body, angle);
+      this.image = loadImage("sprites/wood2.png");
     }
-    display(){
-      var pos =this.body.position;
-      var angle = this.body.angle;
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      fill("red");
-      rect(0, 0, this.width, this.height);
-      pop();
-    }
-  };
-  
+}
